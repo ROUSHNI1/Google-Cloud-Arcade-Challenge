aea197d1504cf787f8a2a00fa6753e7adcc86dc0
Roushni Sinha <79622917+ROUSHNI1@users.noreply.github.com>
2024-02-23 12:03:10 +0000

    updating arcadeers.sh file

--- The Arcade Certification Zone/Automating the Deployment of Infrastructure Using Terraform/arcaders.sh
+++ The Arcade Certification Zone/Automating the Deployment of Infrastructure Using Terraform/arcaders.sh
@@ -8,3 +8,29 @@ terraform --version
 mkdir tfinfra
 
 cd tfinfra
+
+wget https://raw.githubusercontent.com/ROUSHNI1/Google-Cloud-Arcade-Challenge/blob/main/The%20Arcade%20Certification%20Zone/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/mynetwork.tf
+
+wget https://raw.githubusercontent.com/ROUSHNI1/Google-Cloud-Arcade-Challenge/blob/main/The%20Arcade%20Certification%20Zone/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/provider.tf
+
+wget https://raw.githubusercontent.com/ROUSHNI1/Google-Cloud-Arcade-Challenge/blob/main/The%20Arcade%20Certification%20Zone/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/variables.tf
+
+
+mkdir instance
+
+cd instance
+
+
+wget https://raw.githubusercontent.com/ROUSHNI1/Google-Cloud-Arcade-Challenge/blob/main/The%20Arcade%20Certification%20Zone/Automating%20the%20Deployment%20of%20Infrastructure%20Using%20Terraform/instance/main.tf
+
+cd ..
+
+terraform init
+
+terraform fmt
+
+terraform init
+
+
+echo -e "mynet-us-vm\nmynetwork\n$ZONE" | terraform plan -var="instance_name=$(</dev/stdin)" -var="instance_network=$(</dev/stdin)" -var="instance_zone=$(</dev/stdin)"
+echo -e "mynet-us-vm\nmynetwork\n$ZONE" | terraform apply -var="instance_name=$(</dev/stdin)" -var="instance_network=$(</dev/stdin)" -var="instance_zone=$(</dev/stdin)" --auto-approve
